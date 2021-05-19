cee477d52dc522994f319a46a9996ce1bb666ac4
KoxSosen <whyconfused2@gmail.com>
2021-05-19 16:27:04 +0200

    Soontm

--- src/main/java/com/github/koxsosen/commands/WebSearch.java
+++ src/main/java/com/github/koxsosen/commands/WebSearch.java
@@ -3,12 +3,17 @@ package com.github.koxsosen.commands;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
 public class WebSearch implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent messageCreateEvent) {
         if (messageCreateEvent.getMessageContent().equalsIgnoreCase("!g")) {
-            // TODO
+            messageCreateEvent.getMessageContent().toLowerCase(Locale.ROOT);
+                    messageCreateEvent.getMessageContent().trim()
+                    .split("/ +/").toString();
         }
     }
 }
