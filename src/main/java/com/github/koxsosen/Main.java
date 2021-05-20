7b975124a9633b6cdad218579c4879abd8b81e2d
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2021-05-20 20:10:41 +0200

    Dynamic data & config loader rebase
    Big thanks to @B1gLev again!

--- src/main/java/com/github/koxsosen/Main.java
+++ src/main/java/com/github/koxsosen/Main.java
@@ -3,11 +3,10 @@ package com.github.koxsosen;
 import com.github.koxsosen.commands.HelpCommand;
 import com.github.koxsosen.commands.InviteCommand;
 import com.github.koxsosen.commands.WebSearch;
+import com.github.koxsosen.info.Prefix;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
-import com.moandjiezana.toml.Toml;
 
-import java.io.InputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,37 +18,23 @@ public class Main {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    // Define the config file
-    private static final String CONFIG_TOML= "config.toml";
-
     public static void main(String[] args) {
 
-        @SuppressWarnings("InstantiationOfUtilityClass") Main main = new Main();
-        InputStream stream = main.getClass().getClassLoader().getResourceAsStream(CONFIG_TOML);
-        Toml toml = new Toml().read(stream);
         // Set a fallback if Log4j isn't found.
         FallbackLoggerConfiguration.setDebug(true);
 
-        // Get the strings from the table of toml
-        String token = String.valueOf(toml.getTable("essential").getString("token"));
-        String prefix = String.valueOf(toml.getTable("essential").getString("prefix"));
-        String activitytype = String.valueOf(toml.getTable("optional").getString("statusmethod"));
-        String status = String.valueOf(toml.getTable("optional").getString("status"));
-
-       // logger.info("Successfully read the bots token which is" + token);
-
-        logger.info("The bots prefix is " + prefix);
-        logger.info("The bots status is " + status + " and it's method is " + activitytype);
+        logger.info("The bots prefix is " + Prefix.PREFIX());
+        logger.info("The bots status is " + Prefix.STATUS() + " and it's method is " + Prefix.STATUSMETHOD());
 
         // Login using the discord api
         DiscordApi api = new DiscordApiBuilder()
-                .setToken(token)
+                .setToken(Prefix.TOKEN())
                 .login().join();
                 logger.info("Logged in as " + api.getYourself() + ", operating in " + api.getServers().size() + " servers.");
                 api.setReconnectDelay(attempt -> attempt * 2);
 
         // Set the bots status
-        api.updateActivity(ActivityType.valueOf(activitytype), status);
+        api.updateActivity(ActivityType.valueOf(Prefix.STATUSMETHOD()), Prefix.STATUS());
 
         // Register commands
         api.addMessageCreateListener(new HelpCommand());
--- src/main/java/com/github/koxsosen/info/Prefix.java
+++ src/main/java/com/github/koxsosen/info/Prefix.java
@@ -1,12 +1,34 @@
 package com.github.koxsosen.info;
 
+import com.github.koxsosen.Main;
+import com.moandjiezana.toml.Toml;
+
+import java.io.InputStream;
 
 public class Prefix {
 
-    // Temporary solution. - The whole toml management will be moved to here.
+    private static final String CONFIG_TOML= "config.toml";
+
+    static Main main = new Main();
+    static InputStream stream = main.getClass().getClassLoader().getResourceAsStream(CONFIG_TOML);
+    static Toml toml = new Toml().read(stream);
+
     public static String PREFIX() {
-        return "d.";
+        return String.valueOf(toml.getTable("essential").getString("prefix"));
     }
+
+    public static String TOKEN() {
+        return String.valueOf(toml.getTable("essential").getString("token"));
+    }
+
+    public static String STATUSMETHOD() {
+        return String.valueOf(toml.getTable("optional").getString("statusmethod"));
+    }
+
+    public static String STATUS() {
+        return String.valueOf(toml.getTable("optional").getString("status"));
+    }
+
 }
 
 
