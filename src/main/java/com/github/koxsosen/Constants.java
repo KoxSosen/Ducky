25ab2394581e2a00c11527265c4553f26ed88913
Charlie <info@ttr3.eu>
2021-05-22 23:00:09 +0200

    Delete tokan (again)

--- src/main/java/com/github/koxsosen/Constants.java
+++ src/main/java/com/github/koxsosen/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
 
     public static final String PREFIX = "q.";
 
-    public static final String TOKEN = "ODQ1NjcyMjUxMjE0OTIxNzYw.YKkXqA.86So8KGZy3MGQmT9u2x08jOYWe8";
+    public static final String TOKEN = "token";
 
     public static final String STATUS = "q.g | q.help";
 
