b170005529965f72f8c3f34402b0d6332e02f46e
TTRCharlie <mcmbazsi@gmail.com>
2021-05-22 22:59:10 +0200

    Update dependebot config

--- src/main/java/com/github/koxsosen/Constants.java
+++ src/main/java/com/github/koxsosen/Constants.java
@@ -2,15 +2,15 @@ package com.github.koxsosen;
 
 public final class Constants {
 
-    public static final String PREFIX = ".";
+    public static final String PREFIX = "q.";
 
-    public static final String TOKEN = "token";
+    public static final String TOKEN = "ODQ1NjcyMjUxMjE0OTIxNzYw.YKkXqA.86So8KGZy3MGQmT9u2x08jOYWe8";
 
-    public static final String STATUS = ".g | .help";
+    public static final String STATUS = "q.g | q.help";
 
     public static final String STATUSTYPE = "WATCHING";
 
-    public static final String PASTEURL = "https://paste.hahota.net";
+    public static final String PASTEURL = "https://paste.ttr3.eu";
 
     private Constants() { }
 
