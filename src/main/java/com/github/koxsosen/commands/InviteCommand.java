38ea1e509f76d091fff8f6fdf2b1ee13754aaf3c
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2021-06-17 20:04:20 +0200

    Include repo link, and remove preview from InviteCommand.java.

--- src/main/java/com/github/koxsosen/commands/InviteCommand.java
+++ src/main/java/com/github/koxsosen/commands/InviteCommand.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.permission.PermissionType;
 import org.javacord.api.entity.permission.PermissionsBuilder;
 
@@ -18,12 +19,19 @@ public class InviteCommand implements CommandExecutor {
     @Command(aliases = {Constants.PREFIX +"inv"}, async = true, description = "Create an invite for Ducky")
     public void onCommand(TextChannel channel, Message message) {
 
-        channel.sendMessage("**Ducky** - You can invite the bot using the following url: \n"
-                + message.getApi().createBotInvite(new PermissionsBuilder().setAllowed(
+        new MessageBuilder()
+                .append("**Ducky** - You can invite the bot using the following url:")
+                .append("\n <")
+                .append(message.getApi().createBotInvite(new PermissionsBuilder().setAllowed(
                         PermissionType.MANAGE_CHANNELS,
                         PermissionType.SEND_MESSAGES,
                         PermissionType.READ_MESSAGE_HISTORY)
-                .build()));
+                        .build()))
+                .append(">")
+                .append("\nThe bot's source code can be found here:")
+                .append("\n <https://github.com/KoxSosen/Ducky>")
+                .send(channel);
+
         logger.info(message.getAuthor() + " requested this command.");
     }
 }
