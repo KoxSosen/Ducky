9fab76cc7bba87622c450f18e1f43f4c20529ce7
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2021-06-21 18:17:02 +0200

    Remove default values

--- src/main/java/com/github/koxsosen/Constants.java
+++ src/main/java/com/github/koxsosen/Constants.java
@@ -1,22 +1,23 @@
 package com.github.koxsosen;
 
+
 public final class Constants {
 
     public static final String PREFIX = "."; // Prefix
 
     public static final String TOKEN = ""; // This is the loc of the token
 
-    public static final String STATUS = ".g | .help"; // Status
+    public static final String STATUS = ""; // Status
 
-    public static final String STATUSTYPE = "PLAYING"; // WATCHING / PLAYING
+    public static final String STATUSTYPE = ""; // WATCHING / PLAYING
 
-    public static final String PASTEURL = "https://paste.hahota.net"; // Paste url
+    public static final String PASTEURL = ""; // Paste url
 
-    public static final String SCRAPEURL = "https://duckduckgo.com/html/?q="; // The url to scrape, eg https://duckduckgo.com/html/?q=
+    public static final String SCRAPEURL = ""; // The url to scrape, eg https://duckduckgo.com/html/?q=
 
-    public static final String ISSAFESEARCH = "&kp=1";
+    public static final String ISSAFESEARCH = ""; // &kp=1
 
-    public static final String PROXYHOST = "localhost"; // Proxy ip: eg 172.0.0.1
+    public static final String PROXYHOST = ""; // Proxy ip: eg 172.0.0.1
 
     public static final Integer PROXYPORT = 8080; // Proxy port. It's an integer, must be a value. eg 8080 or 8888
 
@@ -49,9 +50,11 @@ public final class Constants {
     public static String PROXYHOST() {
         return PROXYHOST;
     }
+
     public static Integer PROXYPORT() {
         return PROXYPORT;
     }
+
     public static String ISSAFESERACH() { // could be an integer but meh
         return ISSAFESEARCH;
     }
