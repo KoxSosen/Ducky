686a0fd92e9d329a1a2365e2af2947c95aed017f
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2021-11-01 18:34:12 +0100

    Precommit changes from the Dockerfile PR. Thanks for the catch again @CDFN! :p

--- src/main/java/com/github/koxsosen/Main.java
+++ src/main/java/com/github/koxsosen/Main.java
@@ -65,7 +65,7 @@ public class Main {
                 .login().join();
                 // If the bot disconnects always reconnect with a 2*sec delay. ( 1st: 2s, 2nd:4s )
                 api.setReconnectDelay(attempt -> attempt * 2);
-                // Only cache 10 messages per channel & remove ones older than 30 min.
+                // Only cache 10 messages per channel & remove ones older than 15 min.
                 api.setMessageCacheSize(10, 30*30);
         // Set the bots status
         api.updateActivity(ActivityType.valueOf(Constants.STATUSTYPE), Constants.STATUS());
